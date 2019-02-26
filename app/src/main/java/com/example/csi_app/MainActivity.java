35ed9dc004df905f4f40a734300f1b85d3ea1fbf
Your Name Brian <bshuryan@oakland.edu>
2019-02-26 18:45:31 -0500

    test2

--- app/src/main/java/com/example/csi_app/MainActivity.java
+++ app/src/main/java/com/example/csi_app/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 //Testing
 
-
+//test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
